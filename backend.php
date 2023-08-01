d15f4056023e5faee4f2dc4cab30034fa66d4a17
Skullfiredevil <jotipinto@gmail.com>
2023-08-01 11:35:42 +0100

    added php code

---
+++ backend.php
@@ -0,0 +1,25 @@
+$database = [
+    'bases' => [
+      'thin',
+      'deep dish',
+      'stuffed crust'
+  ],
+    'sauces' => [
+        'tomato',
+        'bbq',
+        'chocolate'
+    ],
+    'flavours' => [
+        'cheese',
+        'peperoni',
+        'ham & pineapple',
+        'chicken & bacon',
+        'chocolate & candy'
+    ],
+    'extras' => [
+      'chili oil',
+      'double up',
+      'olives',
+      'dipping oil'
+  ]
+];
